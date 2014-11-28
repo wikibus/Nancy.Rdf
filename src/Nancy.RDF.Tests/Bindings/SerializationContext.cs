f0b60b27c6bfcd80737018c8e144e998536a5ddd
tpluscode <tpluscode@users.noreply.github.com>
2014-11-28 20:21:51 +0100

    mock away IEntitySerializer

--- src/Nancy.RDF.Tests/Bindings/SerializationContext.cs
+++ src/Nancy.RDF.Tests/Bindings/SerializationContext.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using FakeItEasy;
 using JsonLD.Entities;
 
@@ -8,23 +7,11 @@ namespace Nancy.RDF.Tests.Bindings
     public class SerializationContext
     {
         private readonly Stream _outputStream = new MemoryStream();
-        private readonly IContextProvider _contextProvider = A.Fake<IContextProvider>();
-        private readonly IEntitySerializer _serializer;
-
-        public SerializationContext()
-        {
-            A.CallTo(() => _contextProvider.GetContext(A<Type>.Ignored)).Returns(null);
-            _serializer = new EntitySerializer(_contextProvider);
-        }
+        private readonly IEntitySerializer _serializer = A.Fake<IEntitySerializer>();
 
         public Stream OutputStream
         {
             get { return _outputStream; }
-        }
-
-        public IContextProvider ContextProvider
-        {
-            get { return _contextProvider; }
         }
 
         public IEntitySerializer Serializer
