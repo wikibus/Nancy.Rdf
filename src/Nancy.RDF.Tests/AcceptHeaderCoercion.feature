eff5077f71f38e5080d790ebb83e5080a8f03d96
tpluscode <tpluscode@users.noreply.github.com>
2014-12-01 20:51:41 +0100

    tests for convention

---
+++ src/Nancy.RDF.Tests/AcceptHeaderCoercion.feature
@@ -0,0 +1,23 @@
+﻿Feature: Accept Header Coercion
+	To be more dev-friendly
+	I want default to return RDF from routes by default
+	
+Scenario: Set default Accept to Turtle
+	Given default convention
+	When invoke the convention with empty header collection
+	Then Accept header should be 'text/turtle', '1.0'
+	
+Scenario: Set default Accept to declared media type
+	Given convention set to return json-ld
+	When invoke the convention with empty header collection
+	Then Accept header should be 'application/ld+json', '1.0'
+	
+Scenario: Respect existing Accept when overriden with default
+	Given default convention
+	When invoke the convention with non-empty header collection
+	Then Accept header should be 'text/html', '0.9'
+
+Scenario: Respect existing Accept when overriden selected media type
+	Given convention set to return json-ld
+	When invoke the convention with non-empty header collection
+	Then Accept header should be 'text/html', '0.9'
---
+++ src/Nancy.RDF.Tests/Bindings/AcceptHeaderCoercionSteps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nancy.RDF.Conventions;
+using Nancy.RDF.Responses;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Nancy.RDF.Tests.Bindings
+{
+    [Binding]
+    public class AcceptHeaderCoercionSteps
+    {
+        private CoerceAcceptHeaders _convention;
+        private IEnumerable<Tuple<string, decimal>> _coercedAcceptHeaders;
+
+        [Given(@"default convention")]
+        public void GivenDefaultConvention()
+        {
+            _convention = AcceptHeaderConventions.CoerceBlankAcceptHeader();
+        }
+
+        [Given(@"convention set to return json-ld")]
+        public void GivenConventionSetToReturn()
+        {
+            _convention = AcceptHeaderConventions.CoerceBlankAcceptHeader(RdfSerialization.JsonLd);
+        }
+
+        [When(@"invoke the convention with empty header collection")]
+        public void WhenInvokeTheConvention()
+        {
+            _coercedAcceptHeaders = _convention(new Tuple<string, decimal>[0], new NancyContext());
+        }
+
+        [When(@"invoke the convention with non-empty header collection")]
+        public void WhenInvokeTheConventionWithExistingHeader()
+        {
+            _coercedAcceptHeaders = _convention(new[] { Tuple.Create("text/html", 0.9m) }, new NancyContext());
+        }
+
+        [Then(@"Accept header should be '(.*)', '(.*)'")]
+        public void ThenAcceptHeaderShouldBe(string mediaType, decimal weight)
+        {
+            Assert.That(_coercedAcceptHeaders.Single(), Is.EqualTo(Tuple.Create(mediaType, weight)));
+        }
+    }
+}
