5ed39e0111e8beecdc5a47d828c49d856795269d
tpluscode <tpluscode@users.noreply.github.com>
2014-12-06 21:07:19 +0100

    add missing N3 serializer

---
+++ src/Nancy.RDF/Responses/Notation3Serializer.cs
@@ -0,0 +1,27 @@
+﻿using JsonLD.Entities;
+using VDS.RDF.Writing.Formatting;
+
+namespace Nancy.RDF.Responses
+{
+    /// <summary>
+    /// Serializer for Notation3 format
+    /// </summary>
+    public class Notation3Serializer : RdfSerializer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Notation3Serializer"/> class.
+        /// </summary>
+        public Notation3Serializer(IEntitySerializer entitySerializer)
+            : base(RdfSerialization.NTriples, entitySerializer)
+        {
+        }
+
+        /// <summary>
+        /// Creates the triple formatter for Notation3.
+        /// </summary>
+        protected override ITripleFormatter CreateFormatter()
+        {
+            return new Notation3Formatter();
+        }
+    }
+}
