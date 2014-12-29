adb63d3067d4036b0903ad8429186c857e9c8107
tpluscode <tpluscode@users.noreply.github.com>
2014-12-29 18:26:06 +0100

    add installer to wire up namespace mappr

---
+++ src/Nancy.RDF/Installer.cs
@@ -0,0 +1,19 @@
+﻿using Nancy.Bootstrapper;
+using VDS.RDF;
+
+namespace Nancy.RDF
+{
+    /// <summary>
+    /// Installs components required by Nancy.RDF
+    /// </summary>
+    public class Installer : Registrations
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Installer"/> class.
+        /// </summary>
+        public Installer()
+        {
+            Register<INamespaceMapper>(new NamespaceMapper(true));
+        }
+    }
+}
