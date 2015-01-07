4bc87376d950592e71224d930b343680bdccbbc2
tpluscode <tpluscode@users.noreply.github.com>
2015-01-07 15:20:11 +0100

    test serving context

---
+++ src/Nancy.RDF.Tests/ContextModuleTests.cs
@@ -0,0 +1,55 @@
+﻿using CsQuery.ExtensionMethods;
+using FakeItEasy;
+using FluentAssertions;
+using JsonLD.Entities;
+using Nancy.Responses.Negotiation;
+using Nancy.Testing;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Nancy.RDF.Tests
+{
+    public class ContextModuleTests
+    {
+        private Browser _browser;
+
+        [SetUp]
+        public void Setup()
+        {
+            _browser = new Browser(
+                with => with.Module(new ContextModuleTestable())
+                            .Dependency(A.Dummy<IEntitySerializer>()));
+        }
+
+        [Test]
+        public void Should_serve_jsonld_context_by_default()
+        {
+            // given
+            const string expected = "{ 'sch': 'http://schema.org' }";
+
+            // when
+            var response = _browser.Get("/context/someclass");
+
+            // then
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            JToken.DeepEquals(response.ToJSON(), expected).Should().BeTrue();
+        }
+
+        [Test]
+        public void Should_not_serve_jsonld_context_in_other_format()
+        {
+            // when
+            var response = _browser.Get("/context/someclass", with => with.Accept(new MediaRange(RdfSerialization.Turtle.MediaType)));
+
+            // then
+            response.StatusCode.Should().Be(HttpStatusCode.NotAcceptable);
+        }
+
+        public class ContextModuleTestable : ContextModule
+        {
+            public ContextModuleTestable() : base("context")
+            {
+            }
+        }
+    }
+}
---
+++ src/Nancy.RDF/ContextModule.cs
@@ -0,0 +1,23 @@
+﻿namespace Nancy.RDF
+{
+    /// <summary>
+    /// Module, which serves JSON-LD contexts
+    /// </summary>
+    public abstract class ContextModule : NancyModule
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContextModule"/> class.
+        /// </summary>
+        protected ContextModule()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContextModule"/> class.
+        /// </summary>
+        /// <param name="modulePath">A base path for contexts.</param>
+        protected ContextModule(string modulePath) : base(modulePath)
+        {
+        }
+    }
+}
