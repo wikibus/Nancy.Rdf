e56bc97599f016cae58556015f0adac3fa134067
Tomasz Pluskiewicz <tpluskiewicz@pgs-soft.com>
2016-03-04 10:43:26 +0100

    no more nuspec :sparkles: [skip ci]

--- src/Nancy.RDF/Properties/AssemblyInfo.cs
+++ src/Nancy.RDF/Properties/AssemblyInfo.cs
@@ -2,6 +2,8 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Nancy.RDF")]
-[assembly: AssemblyDescription("Nancy components for handling RDF models, etc")]
+[assembly: AssemblyDescription("Nancy components for handling RDF requests and responses")]
+[assembly: AssemblyCompany("Tomasz Pluskiewicz")]
+[assembly: AssemblyCopyright("Copyright © 2014-2016 Tomasz Pluskiewicz")]
 
 [assembly: Guid("1fbbd377-0dc0-4860-8510-59bae87aa7db")]
