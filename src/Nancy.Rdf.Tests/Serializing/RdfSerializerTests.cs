77fdfd820b7e1d299d227661c2ec8e1267d9d443
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2016-03-25 11:15:40 +0100

    rdf serializer tests

---
+++ src/Nancy.Rdf.Tests/Serializing/RdfSerializerTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FakeItEasy;
+using JsonLD.Entities;
+using Nancy.Rdf.Responses;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using VDS.RDF;
+using Vocab;
+
+namespace Nancy.Rdf.Tests.Serializing
+{
+    [TestFixture]
+    public class RdfSerializerTests
+    {
+        private RdfSerializerTestable _serializer;
+        private IEntitySerializer _entitySerializer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _entitySerializer = A.Fake<IEntitySerializer>();
+            _serializer = new RdfSerializerTestable(_entitySerializer);
+        }
+
+        [Test]
+        public void Should_serialize_bool()
+        {
+            // given
+            A.CallTo(() => _entitySerializer.Serialize(A<object>._, A<SerializationOptions>._))
+                .Returns(JObject.Parse(@"{
+  '@context':
+    {
+      'dateCreated': 'http://example.api/o#Issue/dateCreated',
+      'isResolved': 'http://example.api/o#Issue/isResolved'
+    },
+  '@id': 'http://localhost:61186/issues/2',
+  'dateCreated': '2016-03-21T00:00:00',
+  'isResolved': true
+}"));
+
+            // when
+            _serializer.Serialize("text/turtle", new object(), new MemoryStream());
+
+            // then
+            Assert.That(_serializer.Triples.Any(triple =>
+            {
+                var valueMatches = ((LiteralNode)triple.Object).Value == "true";
+                var dataTypeMatches = ((LiteralNode)triple.Object).DataType == new Uri(Xsd.dateTime);
+
+                return valueMatches && dataTypeMatches;
+            }));
+        }
+
+        private class RdfSerializerTestable : RdfSerializer
+        {
+            public RdfSerializerTestable(IEntitySerializer entitySerializer)
+                : base(RdfSerialization.Turtle, entitySerializer)
+            {
+                Triples = new List<Triple>();
+            }
+
+            public IList<Triple> Triples { get; set; }
+
+            protected override void WriteRdf(StreamWriter writer, IEnumerable<Triple> triples)
+            {
+                Triples = triples.ToList();
+            }
+        }
+    }
+}
