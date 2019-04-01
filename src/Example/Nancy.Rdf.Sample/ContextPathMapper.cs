e34f1a24ff932ee1a9b7b0720ba2b3573d7fa18a
tpluscode <tpluscode@users.noreply.github.com>
2019-04-01 20:09:17 +0200

    update sample project

---
+++ src/Example/Nancy.Rdf.Sample/ContextPathMapper.cs
@@ -0,0 +1,10 @@
+﻿namespace Nancy.Rdf.Sample
+{
+    public class ContextPathMapper : Contexts.DefaultContextPathMapper
+    {
+        public ContextPathMapper()
+        {
+            ServeContextOf<Person>();
+        }
+    }
+}
